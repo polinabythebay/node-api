1b849032610bff4ef6641514bc051ee32bb419fc
Polina Soshnin <psoshnin@wellesley.edu>
2015-12-01 17:49:48 -0800

    initial commit

---
+++ README.markdown
@@ -0,0 +1,7 @@
+### Exploring APIs with Node.js
+
+I'm using Node.js + two libraries (https + Underscore.js) to explore APIs and answer interesting questions.
+
+A few questions I'm looking at:
+
+- Looking at the most popular languages in a Github user repo
---
+++ github.js
@@ -0,0 +1,77 @@
+var _ = require('underscore');
+var https = require('https');
+
+/*************************************************************
+Underscore Mixin
+**************************************************************/
+_.mixin({
+    'sortKeysBy': function (obj, comparator) {
+        var keys = _.sortBy(_.keys(obj), function (key) {
+            return comparator ? comparator(obj[key], key) : key;
+        });
+    
+        return _.object(keys, _.map(keys, function (key) {
+            return obj[key];
+        }));
+    }
+});
+
+var languageRank = function(arr) {
+
+  var languages = {};
+
+  _.each(arr, function(item) {
+    if (item.language === null) {
+      if (languages["No Language"]) {
+        ++languages["No Language"];
+      } else {
+        languages["No Language"] = 1;
+      }
+    } else if (languages[item.language]) {
+      ++languages[item.language];
+    } else {
+      languages[item.language] = 1;
+    }
+  });
+
+ return _.sortKeysBy(languages, function (value, key) {
+  //changes from ascending to descending sort
+    return -(value);
+  });
+
+}
+
+/*************************************************************
+Github Repo Data
+**************************************************************/
+
+var options = {
+  "method": "GET",
+  "hostname": "api.github.com",
+  "path": "/users/polinadotio/repos?per_page=100",
+  "headers": {
+    "User-Agent": "polinadotio"
+  }
+};
+
+var repo_data;
+
+var req = https.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    repo_data = JSON.parse(body.toString());
+    console.log("languages\n", languageRank(repo_data));
+  });
+});
+
+req.end();
+
+
+
+
